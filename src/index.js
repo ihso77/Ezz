3196742a130c182c4fbacf3a7308d4fc9a95ae8a
ihso77 <piohio309j@gmail.com>
2025-11-11 22:11:56 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const client = new Client({
-	intens: [
+	intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMembers,
 		GatewayIntentBits.GuildMessages,
